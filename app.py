858bf6c732105ef284ad83bb60e421de835e12fa
mikhailKhmel <mikhail.khmel@gmail.com>
2019-03-11 00:44:11 +0300

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, templating, render_template, redirect, session, url_for, abort
 import sqlite3
+import os
 
 app = Flask(__name__)
 current_user = ''
@@ -176,4 +177,5 @@ def add_newtask():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
